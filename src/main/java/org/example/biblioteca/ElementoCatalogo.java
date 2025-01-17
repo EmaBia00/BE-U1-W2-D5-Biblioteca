d1e67ab639428b3e8ab637cd63c40b316ff481df
Emanuele Bianchi <emanuelebianchi00@gmail.com>
2025-01-17 11:10:36 +0100

    Add ElementoCatalogo

--- src/main/java/org/example/biblioteca/ElementoCatalogo.java
+++ src/main/java/org/example/biblioteca/ElementoCatalogo.java
@@ -1,4 +1,36 @@
 package org.example.biblioteca;
 
-public class ElementoCatalogo {
+public abstract class ElementoCatalogo {
+    private String isbn;
+    private String titolo;
+    private int annoPubblicazione;
+    private int numeroPagine;
+
+    public ElementoCatalogo(String isbn, String titolo, int annoPubblicazione, int numeroPagine) {
+        this.isbn = isbn;
+        this.titolo = titolo;
+        this.annoPubblicazione = annoPubblicazione;
+        this.numeroPagine = numeroPagine;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public String getTitolo() {
+        return titolo;
+    }
+
+    public int getAnnoPubblicazione() {
+        return annoPubblicazione;
+    }
+
+    public int getNumeroPagine() {
+        return numeroPagine;
+    }
+
+    @Override
+    public String toString() {
+        return "ISBN: " + isbn + ", Titolo: " + titolo + ", Anno: " + annoPubblicazione + ", Pagine: " + numeroPagine;
+    }
 }
